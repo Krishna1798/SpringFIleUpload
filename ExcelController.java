1c026786c05b2eb4648fb9a1faf171cbdaa64286
Kaustubh Dongare <kaustubh.dongare@perennialsys.com>
2022-02-09 16:28:48 +0530

    Springboot under debugging

---
+++ ExcelController.java
@@ -0,0 +1,63 @@
+package controller;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import helper.ExcelHelper;
+import message.ResponseMessage;
+import model.Tutorial;
+import service.ExcelService;
+
+@RestController
+@RequestMapping("/api/excel")
+//@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+public class ExcelController {
+
+    @Autowired
+    ExcelService fileService;
+
+    @PostMapping
+    public ResponseEntity<ResponseMessage> uploadFile(@RequestParam("file") MultipartFile file) {
+        String message = "";
+
+        if (ExcelHelper.hasExcelFormat(file)) {
+            try {
+                fileService.save(file);
+
+                message = "Uploaded the file successfully: " + file.getOriginalFilename();
+                return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage(message));
+            } catch (Exception e) {
+                message = "Could not upload the file: " + file.getOriginalFilename() + "!";
+                return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage(message));
+            }
+        }
+
+        message = "Please upload an excel file!";
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ResponseMessage(message));
+    }
+
+    @GetMapping("/tutorials")
+    public ResponseEntity<List<Tutorial>> getAllTutorials() {
+        try {
+            List<Tutorial> tutorials = fileService.getAllTutorials();
+
+            if (tutorials.isEmpty()) {
+                return new ResponscdeEntity<>(HttpStatus.NO_CONTENT);
+            }
+
+            return new ResponseEntity<>(tutorials, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
